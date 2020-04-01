9f885485550f45750edf4b876860c0ccc2759491
manish <mandecent.gupta@gmail.com>
2020-04-01 13:39:12 -0400

    deinterleave logic added

--- lib/VariationAnalyzer/Utils/SnippyUtils.py
+++ lib/VariationAnalyzer/Utils/SnippyUtils.py
@@ -18,5 +18,15 @@ class SnippyUtils:
         os.system(command)
 
     def deinterleave(self, fastq_file):
-        print("bash /kb/module/deps/deinterleave.sh > "+ fastq_file + " /kb/module/work/tmp/f.fastq  /kb/module/work/tmp/r.fastq")
-        os.system("bash /kb/module/deps/deinterleave.sh > "+ fastq_file + " /kb/module/work/tmp/f.fastq  /kb/module/work/tmp/r.fastq")
+        path = "/kb/module/work/tmp/"
+        fastq_1 = open(path + "r.fastq",'w')
+        fastq_2 = open(path + "f.fastq",'w')
+        [fastq_1.write(line) if (i % 8 < 4) else fastq_2.write(line) for i, line in enumerate(open(fastq_file))]
+        fastq_1.close()
+        fastq_2.close()
+    
+        #print("bash /kb/module/deps/deinterleave.sh > "+ fastq_file + " /kb/module/work/tmp/f.fastq  /kb/module/work/tmp/r.fastq")
+        #os.system("bash /kb/module/deps/deinterleave.sh > "+ fastq_file + " /kb/module/work/tmp/f.fastq  /kb/module/work/tmp/r.fastq")
+
+#su = SnippyUtils()
+#su.deinterleave("/home/manish/Desktop/VariationAnalyzer/test_local/workdir/tmp/7b7fa138-4fee-47ae-83e4-d23f7481dc8d.inter.fastq")
